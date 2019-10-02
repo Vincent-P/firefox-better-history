a874e651154cb2b6cb1d11b29b9049ef95a6ddd7
Vincent Parizet <vincent.parizet@hotmail.fr>
2019-10-02 22:45:19 +0200

    Move variables to a separate file | Add some components (buttons, inputs, ...)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload or not okay it needs to be clicked again.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <h1>History</h1>
+            </header>
+            <p>
+                Edit <a>src/App.js</a> and save to reload or not okay it needs to be clicked again.
+            </p>
+            <div className="toolbar">
+                <button className="default-button">Day</button>
+                <button className="default-button">Week</button>
+                <button className="default-button">Month</button>
+                <button className="primary-button">Done</button>
+            </div>
+            <p>
+                <button className="primary-button primary-button--micro">Micro</button>
+            </p>
+            <p>
+                <button className="primary-button primary-button--puffy">Puffy</button>
+            </p>
+            <p className="secondary-text">
+                Edit <code>src/App.js</code> and save to reload or not okay it needs to be clicked again.
+            </p>
+            <form>
+                <label for="name">Name (4 to 8 characters):</label>
+                <input className="default-input" type="text" id="name" name="name" placeholder="Placeholder" required/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
